5f492a7076f5e3c394013ef8cfd8f40c0c2e0c9f
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-10 16:09:19 +0300

    Add files via upload

--- lang/tr/qbank_yetkinlik.php
+++ lang/tr/qbank_yetkinlik.php
@@ -1,1 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  See the LICENSE file in the root directory of this source tree.
 
+/**
+ * Turkish strings for qbank_yetkinlik plugin.
+ *
+ * @package    qbank_yetkinlik
+ * @copyright  2026 Hakan Çiğci {@link https://hakancigci.com.tr}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['competency'] = 'Yetkinlik / Kazanım';
+$string['none'] = 'Yok';
+$string['pluginname'] = 'Soru Bankası Yetkinlik Eşleştirme';
+$string['privacy:metadata'] = 'Soru Bankası Yetkinlik eklentisi herhangi bir kişisel veri depolamaz; sadece sorular ile yetkinlikler arasındaki eşleştirmeleri saklar.';
+$string['selectcompetency'] = 'Yetkinlik seçiniz...';
+$string['yetkinlik'] = 'Yetkinlik';
