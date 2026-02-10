389e8bf8a87d5f6c7c11842e535b9c218525147e
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-10 16:08:46 +0300

    Add files via upload

--- lang/en/qbank_yetkinlik.php
+++ lang/en/qbank_yetkinlik.php
@@ -1,1 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * English strings for qbank_yetkinlik plugin.
+ *
+ * @package    qbank_yetkinlik
+ * @copyright  2026 Hakan Çiğci {@link https://hakancigci.com.tr}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Plugin name strings.
+$string['pluginname'] = 'Question Bank Competency Mapping';
+$string['privacy:metadata'] = 'The Question Bank Competency Mapping plugin does not store any personal data; it only maps questions to competencies.';
+
+// Column headers.
+$string['competency'] = 'Competency';
+$string['yetkinlik'] = 'Competency'; // Sütun internal name (get_name) ile uyum için.
+
+// General strings.
+$string['none'] = 'None';
+$string['selectcompetency'] = 'Select competency...';
