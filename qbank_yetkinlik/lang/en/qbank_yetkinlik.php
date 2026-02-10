3fe194cff4879efdb6ed74338998da4636278922
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-10 15:59:32 +0300

    Delete qbank_yetkinlik directory

--- qbank_yetkinlik/lang/en/qbank_yetkinlik.php
+++
@@ -1,6 +0,0 @@
-<?php
-// This file is part of Moodle - http://moodle.org/
-// Language strings for qbank_yetkinlik plugin.
-
-$string['pluginname'] = 'Competency';
-$string['competency'] = 'Competency';
