cf771d34d7149925cd753dae9bf31154f3faaf89
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-18 18:03:39 +0300

    Add files via upload

--- db/services.php
+++ db/services.php
@@ -1,1 +1,12 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
 
+$functions = [
+    'qbank_yetkinlik_save_question_competency' => [
+        'classname'   => 'qbank_yetkinlik\external\save_question_competency',
+        'methodname'  => 'execute',
+        'description' => 'Soru yetkinliğini kaydeder.',
+        'type'        => 'write',
+        'ajax'        => true,
+    ],
+];
