c5593781710690c21b34bd6676b7e7264812dc8f
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-19 20:36:34 +0300

    Update save_question_competency.php

--- classes/external/save_question_competency.php
+++ classes/external/save_question_competency.php
@@ -1,7 +1,21 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
 /**
+ * External API class for saving question competency mapping.
+ *
  * @package    qbank_yetkinlik
- * @copyright  2026
+ * @copyright  2026 Hakan ÇİĞCİ <hakancigci.com.tr>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -11,47 +25,61 @@ defined('MOODLE_INTERNAL') || die();
 
 use stdClass;
 use context_course;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_value;
 
 /**
- * Soru yetkinliklerini kaydeden dış servis sınıfı.
+ * External service class for saving question competencies.
+ *
+ * @package    qbank_yetkinlik
+ * @copyright  2026 Your Name <your.email@example.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class save_question_competency extends \core_external\external_api {
+class save_question_competency extends external_api {
 
     /**
-     * Parametre tanımlamaları.
+     * Parameter definitions for the execute method.
+     *
+     * @return external_function_parameters
      */
     public static function execute_parameters() {
-        // Namespace içindeki karışıklığı önlemek için tam yol kullanıyoruz
-        return new \core_external\external_function_parameters([
-            'questionid'   => new \core_external\external_value(PARAM_INT, 'Soru ID'),
-            'competencyid' => new \core_external\external_value(PARAM_INT, 'Yetkinlik ID'),
-            'courseid'     => new \core_external\external_value(PARAM_INT, 'Kurs ID'),
+        return new external_function_parameters([
+            'questionid'   => new external_value(PARAM_INT, 'Soru ID'),
+            'competencyid' => new external_value(PARAM_INT, 'Yetkinlik ID'),
+            'courseid'     => new external_value(PARAM_INT, 'Kurs ID'),
         ]);
     }
 
     /**
-     * Yetkinlik kaydetme işlemini yürüten ana metod.
+     * Main method to execute the competency saving process.
+     *
+     * @param int $questionid The ID of the question.
+     * @param int $competencyid The ID of the competency.
+     * @param int $courseid The ID of the course.
+     * @return bool True on success.
      */
     public static function execute($questionid, $competencyid, $courseid) {
         global $DB;
 
-        // Parametre validasyonu
+        // Validate the parameters.
         $params = self::validate_parameters(self::execute_parameters(), [
             'questionid'   => $questionid,
             'competencyid' => $competencyid,
             'courseid'     => $courseid,
         ]);
 
-        // Güvenlik ve Context kontrolü
+        // Security and Context check.
         $context = context_course::instance($params['courseid']);
         self::validate_context($context);
         require_capability('moodle/course:manageactivities', $context);
 
         $table = 'local_yetkinlik_qmap';
-        $record = $DB->get_record($table, [
+        $conditions = [
             'questionid' => $params['questionid'],
-            'courseid'   => $params['courseid']
-        ]);
+            'courseid'   => $params['courseid'],
+        ];
+        $record = $DB->get_record($table, $conditions);
 
         if ($params['competencyid'] == 0) {
             if ($record) {
@@ -75,9 +103,11 @@ class save_question_competency extends \core_external\external_api {
     }
 
     /**
-     * Dönüş değeri tanımlaması.
+     * Return value definition for the execute method.
+     *
+     * @return external_value
      */
     public static function execute_returns() {
-        return new \core_external\external_value(PARAM_BOOL, 'Başarı durumu');
+        return new external_value(PARAM_BOOL, 'Başarı durumu');
     }
 }
