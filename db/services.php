bf06abd8d0cd1f6b82667c7c4293350622a368c6
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-24 19:40:09 +0300

    Update services.php

--- db/services.php
+++ db/services.php
@@ -28,7 +28,7 @@ $functions = [
     'qbank_yetkinlik_save_question_competency' => [
         'classname'   => 'qbank_yetkinlik\external\save_question_competency',
         'methodname'  => 'execute',
-        'description' => 'Soru yetkinliğini kaydeder.',
+        'description' => 'The question records competency.',
         'type'        => 'write',
         'ajax'        => true,
     ],
